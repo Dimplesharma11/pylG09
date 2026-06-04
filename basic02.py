f6bbfe9e33b43297247063db596cad3105bf674d
Dimplesharma11 <dimplesharma3427@gmail.com>
2026-06-04 10:56:55 +0530

    Upload all files

---
+++ basic02.py
@@ -0,0 +1,128 @@
+#1
+# a=int(input("Enter month name (1-12):"))
+
+# if a == 1:
+#    print("January")
+# elif a == 2:
+#    print("February")
+# elif a == 3:
+#    print("March")
+# elif a == 4:
+#    print("April")
+# elif a == 5:
+#    print("May")
+# elif a == 6:
+#    print("June")
+# elif a == 7:
+#    print("July")
+# elif a == 8:
+#    print("August")
+# elif a == 9:
+#    print("September")
+# elif a == 10:
+#    print("October")
+# elif a == 11:
+#    print("November")
+# elif a == 12:
+#    print("December")
+# else:
+#    print("NO month found")
+
+#Question 2
+# X=int(input("Enter first number:"))
+# Y=int(input("Enter second number:"))
+# #1
+# if X==Y:
+#     print("Both numer are equal")
+# elif X!=Y:
+#     print("Both are not equal")
+# #2
+# if X>Y:
+#     print("First number is greater")
+# elif X<Y:
+#     print("second number is greater")            
+# #3
+# if X<=Y:
+#     print("first number is less than equal to second number")
+# elif X>=Y:
+#     print("first number is grater than equal to second number")
+#     #4
+# if X>Y:
+#     print("Dimple")
+# elif X<Y:
+#     print("Sharma")
+# else:
+#     print("no value is greater")
+
+#q6
+
+# a=int(input("Enter numer :"))
+# if a%2==0:
+#     print(f"{a} is even number")
+# else:
+#     print(f"{a} is odd number")
+    
+#q3
+# s1=str(input("enter name:"))
+# s2=str(input("Enter name:"))
+# if s1==s2:
+#     print("Both string are equal")
+# elif s1!=s2:
+#     print("both are not equal")
+# else:
+#     print("Error")
+
+#q4
+# s1=input("Enter first string: ")
+# s2=input("Enter second string:")
+# a1=int(s1)
+# a2=int(s2)
+# if a1==a2:
+#     print("both are equal")
+# elif a1!=a2:
+#     print("both are not equal")
+# else:
+#     print("Error")
+#q5
+
+#2
+#Input 3 numbers and return sum of them.
+# a=int(input("Enter number:"))
+# b=int(input("Enter number:"))
+# c=int(input("Enter number:"))
+# sum=a+b+c
+# print("sum=",sum)
+#..
+# input a number and return square of them 
+# a=int(input("Enter number"))
+# b=int(input("Enter number"))
+# square=a**b
+# print("square=",square)
+# for i in range(1,5):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+#string 
+# s1=str(input("Enter name:"))
+# print(len(s1))
+# for i in range(len(s1)):
+#     print(s1,end=" ")
+# print()
+#multiplication table
+# 
+l1=[22,99,67,90,77,55]
+l1.sort()
+print(l1)
+li.insert(2,[8])
+
+
+
+
+
+       
+      
+
+
+
+
